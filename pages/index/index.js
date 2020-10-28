246586834f6fe3d9d9e759f8fe6ee7f4b12926c8
Jie Tan <laobingm@qq.com>
2020-10-28 10:55:55 +0800

    version

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,33 +1,49 @@
-// pages/index.js
+const app = getApp()
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    version: '',
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-    wx.reportAnalytics('account', {
-      go: '666',
-    });
+  onLoad: function (options) {
+    this.setData({
+      version: app.globalData.version
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
     const updateManager = wx.getUpdateManager()
 
-    updateManager.onCheckForUpdate(function(res) {
+    updateManager.onCheckForUpdate(function (res) {
       // 请求完新版本信息的回调
-      console.log(res.hasUpdate)
+      console.log('是否有新版本', res.hasUpdate)
     })
 
-    updateManager.onUpdateReady(function() {
+    updateManager.onUpdateReady(function () {
       wx.showModal({
-        title: '新版来袭',
+        title: '发现新版本',
         content: '有新版本，是否立即进入？',
-        success: function(res) {
+        success: function (res) {
           if (res.confirm) {
             // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
             updateManager.applyUpdate()
@@ -36,59 +52,44 @@ Page({
       })
     })
 
-    updateManager.onUpdateFailed(function() {
+    updateManager.onUpdateFailed(function () {
       // 新版本下载失败
+      console.warn('新版下载失败')
     })
-
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
